49b358b90f07af59e1c5c5dd2eeac5a980957fd4
Zina Kamel <zena.kamel100@gmail.com>
2021-11-11 15:56:35 +0200

    generating graph from connections list

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
 import React from 'react';
 import ReactFlow from 'react-flow-renderer';
 
-const elements = [
-  {
-    id: '1',
-    type: 'input', // input node
-    data: { label: 'Input Node' },
-    position: { x: 250, y: 25 },
-  },
-  // default node
-  {
-    id: '2',
-    data: { label: <div>Default Node</div> },
-    position: { x: 100, y: 125 },
-  },
-  {
-    id: '3',
-    type: 'output', // output node
-    data: { label: 'Output Node' },
-    position: { x: 250, y: 250 },
-  },
-  {
-    id: '4',
-    data: { label: <div>My Test Node</div> },
-    position: { x: 225, y: 200 },
-  },
-  // animated edge
-  { id: 'e1-2', source: '1', target: '2', animated: true },
-  { id: 'e2-4', source: '2', target: '4' },
-  { id: 'e4-3', source: '4', target: '3' },
-];
+// sample input: [(NoiseGenerator, RollingAverager),(RollingAverager, AveragedNoise),(AveragedNoise, Plot)]
+// sample adjacency list: {'NoiseGenerator': ['RollingAverager'], 'RollingAverager': ['AveragedNoise'], 'AveragedNoise': ['Plot']}
+
+// adjacency list generated from NodeList.py file
+const adjacencyList = {'NoiseGenerator': ['RollingAverager'], 'RollingAverager': ['AveragedNoise'], 'AveragedNoise': ['Plot']}
+
+const adjacencyListKeys = Object.keys(adjacencyList)
+
+// adding all nodes of the graph without duplicates
+const allNodes = Object.keys(adjacencyList)
+for (let [, edgeNodes] of Object.entries(adjacencyList)) {
+  for (let n of edgeNodes) {
+    if (!allNodes.includes(n)) {
+      allNodes.push(n)
+    }
+  }
+}
+
+// generating the elements list having nodes with their properties
+const elements = []
+let i = 0
+for (let node of allNodes) {
+  elements.push({
+    id: node,
+    data: { label: node },
+    position: { x: 100 + 60 * i, y: 125 + 60 * i },
+  })
+  i++;
+}
+
+// generating the edges between every source and target node
+for (let i = 0; i < adjacencyListKeys.length; i++) {
+  const node = adjacencyListKeys[i]
+  const edgeNodes = adjacencyList[node]
+  for (let edgeNode of edgeNodes) {
+    elements.push({ id: `e-${node}-${edgeNode}`, source: node, target: edgeNode, animated: true },)
+  }
+}
 
 export default () => (
-  <div style={{ height: 300 }}>
+  <div style={{ height: 800 }}>
     <ReactFlow elements={elements} />
   </div>
 );
