03116e7ce320f6949bb4506b06a7332ad08a0f8c
Zina Kamel <zena.kamel100@gmail.com>
2021-12-19 22:58:20 +0200

    adding layout support and extra features

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Run sample graph from App.js with npm start
+# LabGraph Monitor
+
+LabGraph Monitor helps visualize the graph built. To run the application, first start the backend server for the sample graph. Example:
+
+```
+python -m yaml_support.examples.simple_viz
+```
+The above code will run the simple_viz example graph 
+
+Then run the web application from labgraph monitor folder using 
+
+```
+npm start
+```
+
+
--- src/App.js
+++ src/App.js
@@ -1,47 +1,154 @@
-import React from 'react';
-import ReactFlow from 'react-flow-renderer';
+import React, { useState, useEffect, useCallback} from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import ReactFlow, { addEdge, MiniMap, Controls, Background, isNode} from 'react-flow-renderer';
+import { dataToConnections, connectionsToNodes, dataToObjects } from './helper';
+import './App.css';
+import dagre from 'dagre';
 
-// sample input: [(NoiseGenerator, RollingAverager),(RollingAverager, AveragedNoise),(AveragedNoise, Plot)]
-// sample adjacency list: {'NoiseGenerator': ['RollingAverager'], 'RollingAverager': ['AveragedNoise'], 'AveragedNoise': ['Plot']}
+// sample connections array: {'NoiseGenerator': ['RollingAverager'], 'RollingAverager': ['AveragedNoise'], 'AveragedNoise': ['Plot']}
 
-// adjacency list generated from NodeList.py file
-const adjacencyList = {'NoiseGenerator': ['RollingAverager'], 'RollingAverager': ['AveragedNoise'], 'AveragedNoise': ['Plot']}
+// dagreGraph helps layout the nodes in the graph 
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
 
-const adjacencyListKeys = Object.keys(adjacencyList)
+const nodeWidth = 172;
+const nodeHeight = 36;
 
-// adding all nodes of the graph without duplicates
-const allNodes = Object.keys(adjacencyList)
-for (let [, edgeNodes] of Object.entries(adjacencyList)) {
-  for (let n of edgeNodes) {
-    if (!allNodes.includes(n)) {
-      allNodes.push(n)
+// setting up the horizontal and vertical options to change the orientation 
+const getLayoutedElements = (elements, direction = 'TB') => {
+  const isHorizontal = direction === 'LR';
+  dagreGraph.setGraph({ rankdir: direction });
+
+  elements.forEach((el) => {
+    if (isNode(el)) {
+      dagreGraph.setNode(el.id, { width: nodeWidth, height: nodeHeight });
+    } else {
+      dagreGraph.setEdge(el.source, el.target);
     }
-  }
-}
+  });
 
-// generating the elements list having nodes with their properties
-const elements = []
-let i = 0
-for (let node of allNodes) {
-  elements.push({
-    id: node,
-    data: { label: node },
-    position: { x: 100 + 60 * i, y: 125 + 60 * i },
-  })
-  i++;
-}
+  dagre.layout(dagreGraph);
 
-// generating the edges between every source and target node
-for (let i = 0; i < adjacencyListKeys.length; i++) {
-  const node = adjacencyListKeys[i]
-  const edgeNodes = adjacencyList[node]
-  for (let edgeNode of edgeNodes) {
-    elements.push({ id: `e-${node}-${edgeNode}`, source: node, target: edgeNode, animated: true },)
-  }
-}
+  return elements.map((el) => {
+    if (isNode(el)) {
+      const nodeWithPosition = dagreGraph.node(el.id);
+      el.targetPosition = isHorizontal ? 'left' : 'top';
+      el.sourcePosition = isHorizontal ? 'right' : 'bottom';
+      el.position = {
+        x: nodeWithPosition.x - nodeWidth / 2 + Math.random() / 1000,
+        y: nodeWithPosition.y - nodeHeight / 2,
+      };
+    }
 
-export default () => (
-  <div style={{ height: 800 }}>
-    <ReactFlow elements={elements} />
+    return el;
+  });
+};
+
+const InteractionGraph = () => {
+  const [node_name, setName] = useState("None");
+  const [connections, setConnections] = useState([]);
+  const [elements, setElements] = useState([]);
+  const [nodeObjects, setNodeObjects] = useState([]);
+  const [type, setType] = useState('None')
+
+  // getting the node that the user is clicking 
+  const onElementClick = (event, element) => {
+    setName(element.id)
+    setType(nodeObjects[element.id].type)
+    console.log("node", node_name)
+  };
+
+  // receiving the messages from server 
+  useEffect(() => {
+    // connecting to the server 
+    const client = new W3CWebSocket('ws://localhost:9000');
+
+    client.onopen = () => {
+      console.log('connected');
+      client.send(JSON.stringify({
+        "api_version": "0.1",
+        "api_request": {
+          "request_id": 1,
+          "start_stream_request": {
+            "stream_id": "LABGRAPH.MONITOR",
+             "labgraph.monitor": {
+             }
+          }
+        }
+     }))
+    };
+
+    client.onmessage = (message) => {
+      const dataFromServer = JSON.parse(message.data);
+      if (dataFromServer.stream_batch){
+        const dataArray = dataFromServer.stream_batch["labgraph.monitor"].samples[0].data; // dataArray is the part of JSON data we are interested in
+        const connections = dataToConnections(dataArray); // generating the connections array 
+        const elements = connectionsToNodes(connections); // changing the connections array to an elements array having the properties of nodes in reactflow
+        const layoutedElements = getLayoutedElements(elements); // using dagreGraph and the getLayoutedElements function defined earlier to layout the nodes
+        const nodeObjects = dataToObjects(dataArray); 
+        setElements(layoutedElements);
+        setConnections(connections);
+        setNodeObjects(nodeObjects);
+        console.log("nodeObjects", nodeObjects);
+        console.log("connection", connections);
+        console.log("server message received ", dataArray)
+      }
+    };
+
+    return () => {
+      client.close();
+    }
+  }, []);
+
+  const onLayout = useCallback(
+    (direction) => {
+      const layoutedElements = getLayoutedElements(elements, direction);
+      setElements(layoutedElements);
+    },
+    [elements]
+  );
+
+  return(
+  <div>
+    <div className='table-div'> 
+      <div className='annotations'>Annotations</div>
+      <div className="controls">
+          <button onClick={() => onLayout('TB')}>vertical layout</button>
+          <button onClick={() => onLayout('LR')}>horizontal layout</button>
+        </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Node</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{node_name}</td>
+            <td>{type}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div style={{ height: 400 }}> 
+    <ReactFlow 
+    elements={elements}
+    onElementClick={onElementClick}
+    >
+      <MiniMap
+        nodeBorderRadius={2}
+      />
+      <Controls />
+      <Background color="#aaa" gap={16} />
+    </ReactFlow>
+    </div>
+
   </div>
-);
+
+  
+  )
+};
+
+export default InteractionGraph;
